11eeff2fa9c2cc132211c72192eac854e0783f97
Sergey Usenko <s.usenko@goodt.me>
2024-07-21 20:52:02 +0700

    init

---
+++ src/main/java/com/example/talisia/TalisiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.talisia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalisiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalisiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/talisia/TalisiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talisia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalisiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
